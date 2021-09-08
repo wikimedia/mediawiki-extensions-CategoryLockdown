23f12354a82606b02b229942b056d024e1013dad
Sophivorus <schenonef@gmail.com>
2021-09-08 08:26:58 -0700

    Initial commit
    Change-Id: I143a5afd75a8474b6470d7e355bee70b24adf6b5

---
+++ CategoryLockdown.php
@@ -0,0 +1,34 @@
+<?php
+
+use MediaWiki\MediaWikiServices;
+
+class CategoryLockdown {
+
+	/**
+	 * Main hook
+	 */
+	public static function onUserCan( $title, $user, $action, &$result ) {
+		global $wgCategoryLockdown;
+
+		$groups = MediaWikiServices::getInstance()->getUserGroupManager()->getUserGroups( $user );
+
+		// Admins can view all
+		if ( in_array( 'sysop', $groups ) ) {
+			return;
+		}
+
+		// If the page is in a protected category and the user is not in the allowed group, hide the page
+		$categories = array_keys( $title->getParentCategories() );
+		foreach ( $categories as $category ) {
+			$category = substr( $category, strpos( $category, ':' ) + 1 );
+			if ( array_key_exists( $category, $wgCategoryLockdown ) && !in_array( $wgCategoryLockdown[ $category ], $groups ) ) {
+				return false;
+			}
+		}
+
+		// Protect the category itself
+		if ( $title->getNamespace() === NS_CATEGORY && array_key_exists( $title->getText(), $wgCategoryLockdown ) && !in_array( $wgCategoryLockdown[ $title->getText() ], $groups )) {
+			return false;
+		}
+	}
+}
