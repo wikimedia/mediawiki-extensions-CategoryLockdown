78c777d111c618236d0e627e8c9efb6688a69232
Sophivorus <schenonef@gmail.com>
2026-07-21 09:23:57 -0500

    Add namespace for compatibility with current versions of MediaWiki
    Also delete eslint because this extension has no JavaScript.
    
    Change-Id: I8be617cffd8b246ffbbcc5e6ccb9824deb45d703

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,12 @@
-/* eslint-env node */
 module.exports = function ( grunt ) {
 	const conf = grunt.file.readJSON( 'extension.json' );
 
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
-	grunt.loadNpmTasks( 'grunt-eslint' );
 
 	grunt.initConfig( {
-		banana: conf.MessagesDirs,
-		eslint: {
-			options: {
-				cache: true
-			},
-			all: [
-				'**/*.js{,on}',
-				'!node_modules/**',
-				'!vendor/**'
-			]
-		}
+		banana: conf.MessagesDirs
 	} );
 
-	grunt.registerTask( 'test', [ 'eslint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
